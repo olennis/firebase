8fe482b29689efd92f2e90b9751f5cb78a1584e8
Donghun kim <kisses217@gmail.com>
2020-10-04 00:24:15 +0900

    :books: docs: 데이터베이스기본 세팅

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [nweet, setNweet] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          placeholder="What's on your mind?"
+          maxLength={120}></input>
+        <input type="submit" value="Nweet"></input>
+      </form>
+    </>
+  );
 };
 
 export default Home;
