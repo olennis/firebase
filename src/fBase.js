a8bff67ae69ce6ed01e8c698a73cdacd3c4df1d5
Donghun kim <kisses217@gmail.com>
2020-10-06 16:25:05 +0900

    :books: docs: 트윗 메세지 렌더

--- src/fBase.js
+++ src/fBase.js
@@ -1,6 +1,6 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/database";
+import "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -16,3 +16,4 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase;
 
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fBase";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (event) => {
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((doc) => {
+      const nweetObj = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
+  const onSubmit = async (event) => {
     event.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet: nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
   };
   const onChange = (event) => {
     const {
@@ -18,9 +39,17 @@ const Home = () => {
           type="text"
           onChange={onChange}
           placeholder="What's on your mind?"
-          maxLength={120}></input>
+          maxLength={120}
+          value={nweet}></input>
         <input type="submit" value="Nweet"></input>
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
